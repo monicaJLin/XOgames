7fc0ba1b70e5431df14d94faf21dfacd1db74b71
monicaJLin <77083550+monicaJLin@users.noreply.github.com>
2021-04-14 11:30:23 +0800

    Add files via upload

---
+++ 0716_XOGame-20201213T061924Z-001/0716_XOGame/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace _0716_XOGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.c1 = new System.Windows.Forms.Label();
+            this.c2 = new System.Windows.Forms.Label();
+            this.c3 = new System.Windows.Forms.Label();
+            this.c4 = new System.Windows.Forms.Label();
+            this.c5 = new System.Windows.Forms.Label();
+            this.c6 = new System.Windows.Forms.Label();
+            this.c7 = new System.Windows.Forms.Label();
+            this.c8 = new System.Windows.Forms.Label();
+            this.c9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // c1
+            // 
+            this.c1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.c1.Font = new System.Drawing.Font("新細明體", 48F);
+            this.c1.Location = new System.Drawing.Point(283, 172);
+            this.c1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(95, 87);
+            this.c1.TabIndex = 0;
+            this.c1.Text = "O";
+            this.c1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.c1.Click += new System.EventHandler(this.c1_Click_1);
+            // 
+            // c2
+            // 
+            this.c2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.c2.Font = new System.Drawing.Font("新細明體", 48F);
+            this.c2.Location = new System.Drawing.Point(375, 172);
+            this.c2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(95, 87);
+            this.c2.TabIndex = 1;
+            this.c2.Text = "O";
+            this.c2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // c3
+            // 
+            this.c3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.c3.Font = new System.Drawing.Font("新細明體", 48F);
+            this.c3.Location = new System.Drawing.Point(467, 172);
+            this.c3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(95, 87);
+            this.c3.TabIndex = 2;
+            this.c3.Text = "O";
+            this.c3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // c4
+            // 
+            this.c4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.c4.Font = new System.Drawing.Font("新細明體", 48F);
+            this.c4.Location = new System.Drawing.Point(283, 260);
+            this.c4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.c4.Name = "c4";
+            this.c4.Size = new System.Drawing.Size(95, 87);
+            this.c4.TabIndex = 3;
+            this.c4.Text = "O";
+            this.c4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // c5
+            // 
+            this.c5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.c5.Font = new System.Drawing.Font("新細明體", 48F);
+            this.c5.Location = new System.Drawing.Point(375, 260);
+            this.c5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.c5.Name = "c5";
+            this.c5.Size = new System.Drawing.Size(95, 87);
+            this.c5.TabIndex = 4;
+            this.c5.Text = "O";
+            this.c5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // c6
+            // 
+            this.c6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.c6.Font = new System.Drawing.Font("新細明體", 48F);
+            this.c6.Location = new System.Drawing.Point(467, 260);
+            this.c6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.c6.Name = "c6";
+            this.c6.Size = new System.Drawing.Size(95, 87);
+            this.c6.TabIndex = 5;
+            this.c6.Text = "O";
+            this.c6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // c7
+            // 
+            this.c7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.c7.Font = new System.Drawing.Font("新細明體", 48F);
+            this.c7.Location = new System.Drawing.Point(283, 348);
+            this.c7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.c7.Name = "c7";
+            this.c7.Size = new System.Drawing.Size(95, 87);
+            this.c7.TabIndex = 6;
+            this.c7.Text = "O";
+            this.c7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // c8
+            // 
+            this.c8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.c8.Font = new System.Drawing.Font("新細明體", 48F);
+            this.c8.Location = new System.Drawing.Point(375, 348);
+            this.c8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.c8.Name = "c8";
+            this.c8.Size = new System.Drawing.Size(95, 87);
+            this.c8.TabIndex = 7;
+            this.c8.Text = "O";
+            this.c8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // c9
+            // 
+            this.c9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.c9.Font = new System.Drawing.Font("新細明體", 48F);
+            this.c9.Location = new System.Drawing.Point(467, 348);
+            this.c9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.c9.Name = "c9";
+            this.c9.Size = new System.Drawing.Size(95, 87);
+            this.c9.TabIndex = 8;
+            this.c9.Text = "O";
+            this.c9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(283, 463);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 48);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "重新(R)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(436, 463);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 48);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Exit(E)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 695);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.c9);
+            this.Controls.Add(this.c8);
+            this.Controls.Add(this.c7);
+            this.Controls.Add(this.c6);
+            this.Controls.Add(this.c5);
+            this.Controls.Add(this.c4);
+            this.Controls.Add(this.c3);
+            this.Controls.Add(this.c2);
+            this.Controls.Add(this.c1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label c1;
+        private System.Windows.Forms.Label c2;
+        private System.Windows.Forms.Label c3;
+        private System.Windows.Forms.Label c4;
+        private System.Windows.Forms.Label c5;
+        private System.Windows.Forms.Label c6;
+        private System.Windows.Forms.Label c7;
+        private System.Windows.Forms.Label c8;
+        private System.Windows.Forms.Label c9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 0716_XOGame-20201213T061924Z-001/0716_XOGame/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0716_XOGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //下棋的動作控制程式
+        Boolean T = false;//下棋次序的切換變數
+        private void c1_Click_1(object sender, EventArgs e)
+        {
+            Label c = (Label)sender;
+            if (c.Text == "")
+            {
+
+                if (T == true)
+                {
+                    c.Text = "X";
+                }
+                else
+                {
+                    c.Text = "O";
+                }
+                T = !T;
+                //呼叫ChkWin
+                if (ChkWin() != "") 
+                { MessageBox.Show( ChkWin() + "手獲勝!"); }
+            }
+        }
+        //設定共用副程式
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.c2.Click += new System.EventHandler(this.c1_Click_1);
+            this.c3.Click += new System.EventHandler(this.c1_Click_1);
+            this.c4.Click += new System.EventHandler(this.c1_Click_1);
+            this.c5.Click += new System.EventHandler(this.c1_Click_1);
+            this.c6.Click += new System.EventHandler(this.c1_Click_1);
+            this.c7.Click += new System.EventHandler(this.c1_Click_1);
+            this.c8.Click += new System.EventHandler(this.c1_Click_1);
+            this.c9.Click += new System.EventHandler(this.c1_Click_1);
+            button1_Click(sender, e);//清除狀態
+
+        }
+        //重玩按鍵
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (Control x in this.Controls)//表單上的每一個控制項
+            {
+                if (x is Label)//如果是標籤物件
+                {
+                    ((Label)x).Text = "";//清掉文字內容
+                }
+            }
+
+        }
+        //檢查是否連線的副程式
+        string ChkWin()
+        {
+            if (c1.Text + c2.Text + c3.Text == "OOO") { return "O"; }
+            if (c4.Text + c5.Text + c6.Text == "OOO") { return "O"; }
+            if (c7.Text + c8.Text + c9.Text == "OOO") { return "O"; }
+            if (c1.Text + c4.Text + c7.Text == "OOO") { return "O"; }
+            if (c2.Text + c5.Text + c8.Text == "OOO") { return "O"; }
+            if (c3.Text + c6.Text + c9.Text == "OOO") { return "O"; }
+            if (c1.Text + c5.Text + c9.Text == "OOO") { return "O"; }
+            if (c3.Text + c5.Text + c7.Text == "OOO") { return "O"; }
+            if (c1.Text + c2.Text + c3.Text == "XXX") { return "X"; }
+            if (c4.Text + c5.Text + c6.Text == "XXX") { return "X"; }
+            if (c7.Text + c8.Text + c9.Text == "XXX") { return "X"; }
+            if (c1.Text + c4.Text + c7.Text == "XXX") { return "X"; }
+            if (c2.Text + c5.Text + c8.Text == "XXX") { return "X"; }
+            if (c3.Text + c6.Text + c9.Text == "XXX") { return "X"; }
+            if (c1.Text + c5.Text + c9.Text == "XXX") { return "X"; }
+            if (c3.Text + c5.Text + c7.Text == "XXX") { return "X"; }
+            return "";///OX都沒有連線,傳回空字串
+
+        }
+        //Exit(ESC)
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
